b1d66c73cce515e9bd9fedab3e5a0dc6bd94b006
Bhumika1133 <bhumikajohri7@gmail.com>
2025-12-02 20:25:48 +0530

    day 20

---
+++ Q40.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char bin[100];
+    int i;
+
+    printf("Enter a binary number: ");
+    scanf("%s", bin);
+
+    for (i = 0; bin[i] != '\0'; i++) {
+        if (bin[i] == '0')
+            bin[i] = '1';
+        else if (bin[i] == '1')
+            bin[i] = '0';
+        else {
+            printf("Invalid binary digit!\n");
+            return 0;
+        }
+    }
+
+    printf("1's Complement = %s\n", bin);
+
+    return 0;
+}
