62ec3f48e666d7d730cfbfed0d1d0d3db604280e
Bhumika1133 <bhumikajohri7@gmail.com>
2025-12-02 21:35:00 +0530

    36 day

---
+++ Q72.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j;
+    int matrix[10][10];
+    int sum = 0;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    // Reading the matrix
+    printf("Enter elements of the matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Calculating sum
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            sum += matrix[i][j];
+        }
+    }
+
+    printf("Sum of all elements in the matrix = %d\n", sum);
+
+    return 0;
+}
