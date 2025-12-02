007bdcdd2d6b1c4be05f94386324c04a86cd735c
Bhumika1133 <bhumikajohri7@gmail.com>
2025-12-02 21:27:31 +0530

    day 34

---
+++ Q67.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, pos, element;
+
+    printf("Enter number of elements in the array: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the position to insert (1 to %d): ", n+1);
+    scanf("%d", &pos);
+
+    printf("Enter the element to insert: ");
+    scanf("%d", &element);
+
+    // Shift elements to the right
+    for (i = n; i >= pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[pos - 1] = element; // insert element
+    n++; // increase size
+
+    printf("Array after insertion: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
